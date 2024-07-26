4160c2d049d82d1e6ccf4ed49a8604eb6053038d
Tayyab Abdullah <tabdullah0801@gmail.com>
2024-07-26 14:55:16 +0500

    Promises

--- promise.js
+++ promise.js
@@ -1,5 +1,13 @@
 
+//Promises in JavaScript
 
-const p = new Promise((resolve, reject) => {
-    
-});
+const p = new Promise((resolve, reject) =>{
+    //Kick off some Async work....
+    //....
+    setTimeout(() => {
+        // resolve(1);
+        reject(new Error('message'));
+    }, 2000)
+})
+
+p.then((result) => console.log(result)).catch(err => console.log('Error:', err.message));
